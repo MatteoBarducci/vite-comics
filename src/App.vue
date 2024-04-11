6fc0e2c19799e41659141f6d2b09d343fde9131c
MatteoBarducci <mbardu1301@gmail.com>
2024-04-11 19:20:03 +0200

    feat: add main's lower section

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <script>
   import AppHeader from './components/AppHeader.vue';
-  import AppMain from './components/AppMain.vue'
+  import AppMain from './components/AppMain.vue';
+  import AppLowerSection from './components/AppLowerSection.vue';
 
 
   export default{
     components: {
       AppHeader,
       AppMain,
+      AppLowerSection,
     }
   }
 </script>
 
 <template>
   <AppHeader></AppHeader>
-  <AppMain></AppMain>
+  <main>
+    <AppMain></AppMain>
+    <AppLowerSection></AppLowerSection>
+  </main>
 </template>
 
 <style lang="scss">
---
+++ src/components/AppLowerSection.vue
@@ -0,0 +1,74 @@
+<script>
+    export default{
+        name: 'AppLowerSection',
+        data() {
+            return{
+                labels: [
+                    {
+                        img: '../assets/img/buy-comics-digital-comics.png',
+                        text: 'DIGITAL COMICS'
+                    },
+                    {
+                        img: '../assets/img/buy-comics-merchandise.png',
+                        text: 'DC MERCHANDISE'
+                    },
+                    {
+                        img: '../assets/img/buy-comics-subscriptions.png',
+                        text: 'SUBSCRIPTION'
+                    },
+                    {
+                        img: '../assets/img/buy-comics-shop-locator.png',
+                        text: 'COMIC SHOP LOCATOR'
+                    },
+                    {
+                        img: '../assets/img/buy-dc-power-visa.svg',
+                        text: 'DC POWER VISA'
+                    }
+                ],
+            }
+        },
+        methods: {
+            getImageUrl(name) {
+                return new URL(`${name}`, import.meta.url).href
+            }
+        }
+    }
+</script>
+
+<template>
+    <section>
+        <div class="main-container">
+            <div v-for="label in labels">
+                <img :src="getImageUrl(label.img)" alt="">
+                <span>{{ label.text }}</span>
+            </div>
+
+        </div>
+    </section>
+</template>
+
+<style scoped lang="scss">
+@use '../style/partials/variables.scss' as *;
+
+    section{
+        background-color: $brand-primary;
+    }
+
+    .main-container{
+        display: flex;
+        padding: 44px;
+        color: white;
+
+        div{
+            display: flex;
+            align-items: center;
+            margin: 0 auto;
+
+            img{
+                max-height: 55px;
+                max-width: 55px;
+            }
+        }
+    }
+
+</style>
