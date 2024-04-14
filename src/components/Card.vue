ebbd31cbf20e584dc5285c1677834bf96a188608
MatteoBarducci <mbardu1301@gmail.com>
2024-04-14 23:12:08 +0200

    feat: add dynamic cards

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,7 @@
 <script>
     export default{
         name: 'Card',
+        props: ['image', 'text'],
         data() {
             return{
 
@@ -10,13 +11,29 @@
 </script>
 
 <template>
-    <div class="main-container">
-        ciao
+    <div class="card">
+        <img :src="image" >
+        <figcaption>{{ text }}</figcaption>
+        
     </div>
 </template>
 
 <style scoped lang="scss">
 @use '../style/partials/variables.scss' as *;
 
+    .card{
+        width: 15%; 
+        
+        img{
+            width: 100%;
+            height: 189px;
+            object-fit: cover;
+
+        }
+        figcaption{
+            margin-top: 15px;
+            margin-bottom: 10px;
+        }
+    }
 
 </style>
